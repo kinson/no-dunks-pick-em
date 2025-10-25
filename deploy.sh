366ace67a6e0a8b12b94761b14913cab3fef174d
kinson <1526888+kinson@users.noreply.github.com>
2025-10-25 14:35:40 -0700

    Update deployment resources

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ source .env
 export MIX_ENV=prod
 
 echo "Stopping application"
-_build/prod/rel/classic_clips/bin/classic_clips stop
+_build/prod/rel/no_dunks_pick_em/bin/no_dunks_pick_em stop
 
 echo "Fetching latest deps"
 mix deps.get --only prod
@@ -15,4 +15,4 @@ echo "Migrating"
 mix ecto.migrate
 
 echo "Starting application"
-_build/prod/rel/classic_clips/bin/classic_clips daemon
+_build/prod/rel/no_dunks_pick_em/bin/no_dunks_pick_em daemon
